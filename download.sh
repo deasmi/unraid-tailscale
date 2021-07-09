5ed8510c6e2afaa6e6f525e0b86a67f93dd28a4e
Dean Smith <dean@zelotus.com>
2021-07-09 11:08:53 +0100

    Making download script throw errors

--- download.sh
+++ download.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-VERSION=1.10.1
+VERSION=1.8.8
 TSFILE=tailscale_${VERSION}_amd64.tgz
 PACKAGE="https://pkgs.tailscale.com/stable/${TSFILE}"
 
 echo "Downloading $PACKAGE"
 wget  $PACKAGE >/dev/null 2>&1
 
+if (($?!=200))  
+then 
+	echo >2 "Could not download file ${TSFILE}"
+	exit 1 
+fi
 
 echo "Unpacking"
 DIR=$(mktemp -d -p .)
